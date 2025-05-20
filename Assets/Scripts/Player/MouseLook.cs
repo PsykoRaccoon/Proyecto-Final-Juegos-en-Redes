c5a9f15deb69dd74a004a626ad5ebb7e2474aca3
Victor Sanchez <vickoss9004@gmail.com>
2025-05-19 19:32:04 -0600

    Sistema de tercera persona
    Ps no esta tan chingon, pero funciona :)

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
@@ -7,22 +5,44 @@ public class MouseLook : MonoBehaviour
     public float sensitivity;
     public Transform playerBody;
 
-    float xRotation;
+    private float xRotation = 0f;
+
+    private bool isThirdPerson = false;
+
+    public Vector3 firstPersonPosition;
+    public Vector3 thirdPersonPosition; 
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        transform.localPosition = firstPersonPosition;
     }
 
     void Update()
+    {
+        HandleViewToggle();
+        HandleLook();
+    }
+
+    void HandleViewToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            isThirdPerson = !isThirdPerson;
+            transform.localPosition = isThirdPerson ? thirdPersonPosition : firstPersonPosition;
+        }
+    }
+
+    void HandleLook()
     {
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
